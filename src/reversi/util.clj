bb1b50e02b35bd7912a48c16cea7e38089fec62d
Trever Anderson <treveranderson01@gmail.com>
2022-06-27 22:50:58 -0500

    defined mem struct, wrote some funcs

--- src/reversi/util.clj
+++ src/reversi/util.clj
@@ -1,5 +1,8 @@
 (ns reversi.util)
 
+(import
+  '(java.awt Color))
+
 (defn half
   [num]
   (float (/ num 2)))
@@ -9,3 +12,15 @@
   (vec (apply concat (map (fn [mi]
                             (mapv (fn [ni]
                                     [mi ni]) n)) m))))
+
+(defn gen-grid
+  [m n def-val]
+  (vec (repeat m (vec (repeat n def-val)))))
+
+(defn change-square
+  [grid r c val]
+  (assoc grid r (assoc (grid r) c val)))
+
+(defn lookup
+  [grid r c]
+  ((grid r) c))
