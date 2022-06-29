7c90d0bb162e8c994244f0d4e656199a089fa38e
Trever Anderson <treveranderson01@gmail.com>
2022-06-28 22:43:48 -0500

    starting grid defined

--- src/reversi/util.clj
+++ src/reversi/util.clj
@@ -18,19 +18,19 @@
   (vec (repeat m (vec (repeat n def-val)))))
 
 (defn change-square
-  [grid r c val]
+  [grid [r c] val]
   (assoc grid r (assoc (grid r) c val)))
 
 (defn lookup
-  [grid r c]
+  [grid [r c]]
   ((grid r) c))
 
 (defn index-value
   [grid]
   (let [nums (range (count grid))]
-    (for [x nums
-          y nums]
-      [[x y] (lookup grid x y)])))
+    (for [r nums
+          c nums]
+      [[r c] (lookup grid [r c])])))
 
 (defn locations
   [grid predicate]
@@ -41,3 +41,12 @@
   [grid color]
   (locations grid (fn [_ v]
                     (= v (grid-value color)))))
+
+(defn start-grid
+  ([]
+   (start-grid 8))
+  ([dim]
+  (-> (gen-grid dim dim {}) (change-square [3 3] (grid-value (. Color blue)))
+      (change-square [3 4] (grid-value (. Color orange)))
+      (change-square [4 3] (grid-value (. Color orange)))
+      (change-square [4 4] (grid-value (. Color blue))))))
